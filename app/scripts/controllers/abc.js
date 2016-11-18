cc3f4b4b577538f4f028027b1a47f33de1f90a2d
Lsm <1627403661@qq.com>
2016-11-18 11:08:33 +0800

    qwe

--- app/scripts/controllers/abc.js
+++ app/scripts/controllers/abc.js
@@ -8,52 +8,49 @@
  * Controller of the yoApp
  */
 angular.module('yoApp')
-  .directive('auto',function($http){
-  	return{
-  		restrict:'ECMA',
-  		template:'<div><ul><li ng-repeat="a in aa">{{a.title | limitTo:5}}{{a.title.length>5?".....":""}}</li><p ng-repeat="b in bb"><img ng-src="{{b.img}}" alt="..."></p></ul></div>',
-  		replace:true,
-  		scope:true,
-  		link:function(s,e,attr){
-	  	$http({
-	  		url:'http://www.somenote.cn:1510/test',
-	  		method:'get'
-	  	}).success(function(e){
-	  		s.aa=e
-	  	}),
-	  	$http({
-	  		url:'http://www.somenote.cn:1510/aut',
-	  		method:'get'
-	  	}).success(function(e){
-	  		s.bb=e
-	  	})
-	  	
-	  	
-  		}
-  	}
-  	
-  })
-  .directive('auto1',function($http){
-  	return{
-  		restrict:'ECMA',
-  		template:'<div><ul><li ng-repeat="a in aa">{{a.title | limitTo:5}}{{a.title.length>5?".....":""}}</li></ul><p ng-repeat="b in bb"><img ng-src="{{b.img}}" alt="..."></div>',
-  		replace:true,
-  		scope:true,
-  		link:function(s,e,attr){
-	  	$http({
-	  		url:'http://www.somenote.cn:1510/test2',
-	  		method:'get'
-	  	}).success(function(e){
-	  		s.aa=e
-	  	}),
-	  	$http({
-	  		url:'http://www.somenote.cn:1510/aut',
-	  		method:'get'
-	  	}).success(function(e){
-	  		s.bb=e
-	  	})
-	  	
-  		}
-  	}
-  	
-  })
+  
+  .controller("con",function($scope,$http,getdata){
+	getdata.ajax('http://www.somenote.cn:1510/test','get',function(data){
+		console.log(data)
+		$scope.data=data
+	})
+	getdata.ajax('http://www.somenote.cn:1510/test2','get',function(data1){
+		console.log(data1)
+		$scope.data1=data1
+	})
+	getdata.ajax('http://www.somenote.cn:1510/aut','get',function(data2){
+		console.log(data2)
+		$scope.data2=data2
+	})
+	
+	
+
+})
+.directive("auto",function(){
+	return{
+		restrict:'AEMC', 
+		template:'<div class="da"><li ng-repeat="i in d">{{i.title | limitTo:5}}{{i.title.length>5?".....":""}}</li><li ng-repeat="b in x">{{b.title | limitTo:5}}{{i.title.length>5?".....":""}}</li></div><div ><img  ng-repeat="t in s" ng-src="{{t.img}}" /></div>',
+		scope:true,
+		transclude: true,
+		scope:{d:"=data",s:"=data2",x:"=data1"},
+		link:function(s,e,attr){
+			s.a=attr['b']
+		}
+		
+	}
+})
+
+.service('getdata',function($http){
+	return{
+		ajax:function(url,method,callback){
+			$http({
+				url:url,
+				method:method
+			}).success(function(e){
+				callback(e)
+			})
+		}
+	}
+})
+
+  
